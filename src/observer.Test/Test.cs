8f18f83dfef66bb6c753f9b4e61f0cfb6646383d
DouglasSouza05 <douglasbsouza01@gmail.com>
2023-11-19 18:48:53 -0300

    Estrutura com C# e MSTest

---
+++ src/observer.Test/Test.cs
@@ -0,0 +1,12 @@
+namespace observer.Test;
+
+[TestClass]
+public class MeuTeste
+{
+    [TestMethod]
+    public void MeuMetodoDeTeste()
+    {
+        // Seu código de teste aqui
+        Assert.IsTrue(true);
+    }
+}
---
+++ src/observer/Main.cs
@@ -0,0 +1,10 @@
+﻿namespace observer
+{
+    public class Class1
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
